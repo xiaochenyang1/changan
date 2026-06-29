ffe682d9aaaa6633857158333e97c7ed2cb52f72
肖晨洋 <13939510387@163.com>
2026-06-29 16:56:41 +0800

    feat: 重构总装下线和入库表格为 dtd-table 组件，优化样式和结构

--- src/views/lanhu_zongzhuangxiaxianwaiguanxiaxian/index.vue
+++ src/views/lanhu_zongzhuangxiaxianwaiguanxiaxian/index.vue
@@ -27,117 +27,20 @@
                 src="./assets/img/SketchPng8fc60584944abddb2055cfc737393b63209feba3abb8eeb086c255938d455cc8.png"
               />
             </div>
-            <div class="box_2 flex-row justify-between">
-              <img
-                class="image_3"
-                referrerpolicy="no-referrer"
-                src="./assets/img/SketchPngbdc3705c69a7d4dcc45cf97417ce22b30ccda72b5168416cc8f87870e0d77cf0.png"
-              />
-              <div class="block_3 flex-col">
-                <div class="group_6 flex-row">
-                  <span class="text_5">车型</span>
-                  <span class="text_6">昨日</span>
-                  <span class="text_7">月累</span>
-                  <span class="text_8">年累</span>
-                  <img
-                    class="image_4"
-                    referrerpolicy="no-referrer"
-                    src="./assets/img/SketchPngbdc3705c69a7d4dcc45cf97417ce22b30ccda72b5168416cc8f87870e0d77cf0.png"
-                  />
-                  <img
-                    class="image_5"
-                    referrerpolicy="no-referrer"
-                    src="./assets/img/SketchPng4817c4be228fc7bc18ede833d7238d7920bb1d1d3d2670dd8013f56de51a9992.png"
-                  />
-                  <img
-                    class="image_6"
-                    referrerpolicy="no-referrer"
-                    src="./assets/img/SketchPng4817c4be228fc7bc18ede833d7238d7920bb1d1d3d2670dd8013f56de51a9992.png"
-                  />
-                  <img
-                    class="image_7"
-                    referrerpolicy="no-referrer"
-                    src="./assets/img/SketchPng64b3147c45f4837fc2c2cd6b197350d24fe646de398b381b30369ab685d5fb87.png"
-                  />
-                </div>
-                <div class="text-wrapper_1 flex-row">
-                  <span class="text_9">阿维塔&nbsp;07</span>
-                  <span class="text_10">108</span>
-                  <span class="text_11">108</span>
-                  <span class="text_12">380</span>
-                </div>
-                <div class="text-wrapper_2 flex-row">
-                  <span class="text_13">深蓝&nbsp;L06</span>
-                  <span class="text_14">130</span>
-                  <span class="text_15">130</span>
-                  <span class="text_16">430</span>
-                </div>
-                <div class="text-wrapper_3 flex-row">
-                  <span class="text_17">深蓝&nbsp;S05</span>
-                  <span class="text_18">102</span>
-                  <span class="text_19">102</span>
-                  <span class="text_20">102</span>
-                </div>
-                <div class="text-wrapper_4 flex-row">
-                  <span class="text_21">深蓝&nbsp;S05（海外）</span>
-                  <span class="text_22">0</span>
-                  <span class="text_23">2</span>
-                  <span class="text_24">2</span>
-                </div>
-                <div class="text-wrapper_5 flex-row">
-                  <span class="text_25">启源&nbsp;E07</span>
-                  <span class="text_26">1</span>
-                  <span class="text_27">20</span>
-                  <span class="text_28">20</span>
-                </div>
-                <div class="text-wrapper_6 flex-row">
-                  <span class="text_29">启源&nbsp;E07（海外）</span>
-                  <span class="text_30">700</span>
-                  <span class="text_31">2000</span>
-                  <span class="text_32">2000</span>
-                </div>
+            <div class="dtd-table">
+              <div class="dtd-table__header">
+                <div class="dtd-table__cell dtd-table__cell--header">车型</div>
+                <div class="dtd-table__cell dtd-table__cell--header">昨日</div>
+                <div class="dtd-table__cell dtd-table__cell--header">月累</div>
+                <div class="dtd-table__cell dtd-table__cell--header">年累</div>
+              </div>
+              <div v-for="(row, i) in zongzhuangData" :key="i" class="dtd-table__row">
+                <div class="dtd-table__cell">{{ row.name }}</div>
+                <div class="dtd-table__cell dtd-table__cell--center">{{ row.yesterday }}</div>
+                <div class="dtd-table__cell dtd-table__cell--center">{{ row.month }}</div>
+                <div class="dtd-table__cell dtd-table__cell--center">{{ row.year }}</div>
               </div>
             </div>
-            <img
-              class="image_8"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPng442d6619136ba565ad6a953cc6bc444b69a00b0f7d6a8ac2d52c6bfb1631fc0d.png"
-            />
-            <img
-              class="image_9"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPng442d6619136ba565ad6a953cc6bc444b69a00b0f7d6a8ac2d52c6bfb1631fc0d.png"
-            />
-            <img
-              class="image_10"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPngcfa5e1ed9051a4733a3b8fd158c311634ae27a88b04f80b9bbceaafb5b01958b.png"
-            />
-            <img
-              class="image_11"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPngcfa5e1ed9051a4733a3b8fd158c311634ae27a88b04f80b9bbceaafb5b01958b.png"
-            />
-            <img
-              class="image_12"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPngcfa5e1ed9051a4733a3b8fd158c311634ae27a88b04f80b9bbceaafb5b01958b.png"
-            />
-            <img
-              class="image_13"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPngcfa5e1ed9051a4733a3b8fd158c311634ae27a88b04f80b9bbceaafb5b01958b.png"
-            />
-            <img
-              class="image_14"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPngcfa5e1ed9051a4733a3b8fd158c311634ae27a88b04f80b9bbceaafb5b01958b.png"
-            />
-            <img
-              class="image_15"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPngcfa5e1ed9051a4733a3b8fd158c311634ae27a88b04f80b9bbceaafb5b01958b.png"
-            />
           </div>
           <div class="group_7 flex-col">
             <div class="image-wrapper_2 flex-col">
@@ -156,117 +59,20 @@
               入库
               <br />
             </span>
-            <div class="box_3 flex-row justify-between">
-              <img
-                class="image_16"
-                referrerpolicy="no-referrer"
-                src="./assets/img/SketchPngbdc3705c69a7d4dcc45cf97417ce22b30ccda72b5168416cc8f87870e0d77cf0.png"
-              />
-              <div class="group_8 flex-col">
-                <div class="block_4 flex-row">
-                  <span class="text_33">车型</span>
-                  <span class="text_34">昨日</span>
-                  <span class="text_35">月累</span>
-                  <span class="text_36">年累</span>
-                  <img
-                    class="image_17"
-                    referrerpolicy="no-referrer"
-                    src="./assets/img/SketchPngbdc3705c69a7d4dcc45cf97417ce22b30ccda72b5168416cc8f87870e0d77cf0.png"
-                  />
-                  <img
-                    class="image_18"
-                    referrerpolicy="no-referrer"
-                    src="./assets/img/SketchPng4817c4be228fc7bc18ede833d7238d7920bb1d1d3d2670dd8013f56de51a9992.png"
-                  />
-                  <img
-                    class="image_19"
-                    referrerpolicy="no-referrer"
-                    src="./assets/img/SketchPng4817c4be228fc7bc18ede833d7238d7920bb1d1d3d2670dd8013f56de51a9992.png"
-                  />
-                  <img
-                    class="image_20"
-                    referrerpolicy="no-referrer"
-                    src="./assets/img/SketchPng64b3147c45f4837fc2c2cd6b197350d24fe646de398b381b30369ab685d5fb87.png"
-                  />
-                </div>
-                <div class="text-wrapper_7 flex-row">
-                  <span class="text_37">阿维塔&nbsp;07</span>
-                  <span class="text_38">108</span>
-                  <span class="text_39">108</span>
-                  <span class="text_40">380</span>
-                </div>
-                <div class="text-wrapper_8 flex-row">
-                  <span class="text_41">深蓝&nbsp;L06</span>
-                  <span class="text_42">130</span>
-                  <span class="text_43">130</span>
-                  <span class="text_44">430</span>
-                </div>
-                <div class="text-wrapper_9 flex-row">
-                  <span class="text_45">深蓝&nbsp;S05</span>
-                  <span class="text_46">102</span>
-                  <span class="text_47">102</span>
-                  <span class="text_48">102</span>
-                </div>
-                <div class="text-wrapper_10 flex-row">
-                  <span class="text_49">深蓝&nbsp;S05（海外）</span>
-                  <span class="text_50">0</span>
-                  <span class="text_51">2</span>
-                  <span class="text_52">2</span>
-                </div>
-                <div class="text-wrapper_11 flex-row">
-                  <span class="text_53">启源&nbsp;E07</span>
-                  <span class="text_54">1</span>
-                  <span class="text_55">20</span>
-                  <span class="text_56">20</span>
-                </div>
-                <div class="text-wrapper_12 flex-row">
-                  <span class="text_57">启源&nbsp;E07（海外）</span>
-                  <span class="text_58">700</span>
-                  <span class="text_59">2000</span>
-                  <span class="text_60">2000</span>
-                </div>
+            <div class="dtd-table">
+              <div class="dtd-table__header">
+                <div class="dtd-table__cell dtd-table__cell--header">车型</div>
+                <div class="dtd-table__cell dtd-table__cell--header">昨日</div>
+                <div class="dtd-table__cell dtd-table__cell--header">月累</div>
+                <div class="dtd-table__cell dtd-table__cell--header">年累</div>
+              </div>
+              <div v-for="(row, i) in rukuData" :key="i" class="dtd-table__row">
+                <div class="dtd-table__cell">{{ row.name }}</div>
+                <div class="dtd-table__cell dtd-table__cell--center">{{ row.yesterday }}</div>
+                <div class="dtd-table__cell dtd-table__cell--center">{{ row.month }}</div>
+                <div class="dtd-table__cell dtd-table__cell--center">{{ row.year }}</div>
               </div>
             </div>
-            <img
-              class="image_21"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPng442d6619136ba565ad6a953cc6bc444b69a00b0f7d6a8ac2d52c6bfb1631fc0d.png"
-            />
-            <img
-              class="image_22"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPng442d6619136ba565ad6a953cc6bc444b69a00b0f7d6a8ac2d52c6bfb1631fc0d.png"
-            />
-            <img
-              class="image_23"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPngcfa5e1ed9051a4733a3b8fd158c311634ae27a88b04f80b9bbceaafb5b01958b.png"
-            />
-            <img
-              class="image_24"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPngcfa5e1ed9051a4733a3b8fd158c311634ae27a88b04f80b9bbceaafb5b01958b.png"
-            />
-            <img
-              class="image_25"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPngcfa5e1ed9051a4733a3b8fd158c311634ae27a88b04f80b9bbceaafb5b01958b.png"
-            />
-            <img
-              class="image_26"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPngcfa5e1ed9051a4733a3b8fd158c311634ae27a88b04f80b9bbceaafb5b01958b.png"
-            />
-            <img
-              class="image_27"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPngcfa5e1ed9051a4733a3b8fd158c311634ae27a88b04f80b9bbceaafb5b01958b.png"
-            />
-            <img
-              class="image_28"
-              referrerpolicy="no-referrer"
-              src="./assets/img/SketchPngcfa5e1ed9051a4733a3b8fd158c311634ae27a88b04f80b9bbceaafb5b01958b.png"
-            />
           </div>
         </div>
         </div>
@@ -278,6 +84,26 @@
 // 总装下线-外观下线弹框组件。由父级看板通过 visible 控制显示，关闭时 emit('close')。
 defineProps<{ visible: boolean }>()
 const emit = defineEmits<{ (e: 'close'): void }>()
+
+// 总装下线表格数据
+const zongzhuangData = [
+  { name: '阿维塔 07', yesterday: 108, month: 108, year: 380 },
+  { name: '深蓝 L06', yesterday: 130, month: 130, year: 430 },
+  { name: '深蓝 S05', yesterday: 102, month: 102, year: 102 },
+  { name: '深蓝 S05（海外）', yesterday: 0, month: 2, year: 2 },
+  { name: '启源 E07', yesterday: 1, month: 20, year: 20 },
+  { name: '启源 E07（海外）', yesterday: 700, month: 2000, year: 2000 },
+]
+
+// 入库表格数据（与总装下线相同）
+const rukuData = [
+  { name: '阿维塔 07', yesterday: 108, month: 108, year: 380 },
+  { name: '深蓝 L06', yesterday: 130, month: 130, year: 430 },
+  { name: '深蓝 S05', yesterday: 102, month: 102, year: 102 },
+  { name: '深蓝 S05（海外）', yesterday: 0, month: 2, year: 2 },
+  { name: '启源 E07', yesterday: 1, month: 20, year: 20 },
+  { name: '启源 E07（海外）', yesterday: 700, month: 2000, year: 2000 },
+]
 </script>
 
 
