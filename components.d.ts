d14ddf5c75b56f3a8b2e3268b46577ef0efb733a
肖晨洋 <13939510387@163.com>
2026-07-02 16:34:56 +0800

    feat: 添加 Element Plus 组件到全局组件声明

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,12 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     DashboardHeader: typeof import('./src/components/DashboardHeader/index.vue')['default']
+    ElButton: typeof import('element-plus/es')['ElButton']
+    ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
+    ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
+    ElInput: typeof import('element-plus/es')['ElInput']
+    ElOption: typeof import('element-plus/es')['ElOption']
+    ElSelect: typeof import('element-plus/es')['ElSelect']
     Loading: typeof import('./src/components/Loading/index.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
